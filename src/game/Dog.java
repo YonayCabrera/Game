04ca3c12eb099e573cd0b682c50a1073f24d0ffc
Yonay Cabrera <yonaycl@gmail.com>
2016-09-15 09:47:54 +0100

    create enemy and start to thing how getDamage the enemy and the character

---
+++ src/game/Dog.java
@@ -0,0 +1,15 @@
+package game;
+
+
+public class Dog extends Enemy {
+    private final int actualHP;
+    private final int attack;
+    private final int defense;
+
+    public Dog(int actualHP, int attack, int defense) {
+
+        this.actualHP = actualHP;
+        this.attack = attack;
+        this.defense = defense;
+    }
+}
---
+++ test/game/Enemy_will.java
@@ -0,0 +1,12 @@
+package game;
+
+
+import org.junit.Test;
+
+public class Enemy_will {
+    @Test
+    public void Attack(){
+        Enemy enemy = new Dog(200,5,5);
+
+    }
+}
