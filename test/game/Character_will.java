d89ffa4975b35f7ae7e2fbaba59b9aa35f9d38ee
Yonay Cabrera <yonaycl@gmail.com>
2016-09-15 10:39:26 +0100

    do before in test with character green

--- test/game/Character_will.java
+++ test/game/Character_will.java
@@ -1,5 +1,6 @@
 package game;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -7,20 +8,26 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Character_will {
+
+    private Character character;
+
+    @Before
+    public void setUp() throws Exception {
+        character = new Character(200,200,5,5);
+    }
+
     @Test
     public void take_Red_Potion() {
 
         Potion red = new Red(200);
-        Character character = new Character(200,200,5,5);
         character.addItem(red);
-        assertEquals(200,character.getHp(red));
+        assertEquals(200, character.getHp(red));
 
     }
     @Test
     public void take_Blue_Potion() {
 
         Potion blue = new Blue(200);
-        Character character = new Character(200,200,5,5);
         character.addItem(blue);
         assertEquals(200,character.getSP(blue));
 
@@ -29,7 +36,6 @@ public class Character_will {
     public void take_One_Sword() {
 
         Arm sword = new Sword(15,10);
-        Character character = new Character(200,200,5,5);
         character.addItem(sword);
         assertEquals(20,character.getArm(sword));
         assertEquals(10,sword.resistance());
@@ -38,7 +44,6 @@ public class Character_will {
     public void take_One_Arc() {
 
         Arm bow = new Bow(15,10);
-        Character character = new Character(200,200,5,5);
         character.addItem(bow);
         assertEquals(20,character.getArm(bow));
         assertEquals(10,bow.resistance());
@@ -47,7 +52,6 @@ public class Character_will {
     public void put_One_Armor() {
 
         Armor armor = new Armor(20,10);
-        Character character = new Character(200,200,5,5);
         character.addItem(armor);
         assertEquals(25,character.getArmor(armor));
         assertEquals(10,armor.resistance());
@@ -55,14 +59,12 @@ public class Character_will {
     }
     @Test
     public void put_In_Inventory() {
-        Character character = new Character(200,200,5,5);
         character.addItem(new Bow(15,15));
         assertEquals(1,character.size());
 
     }
     @Test
     public void get_Arm() {
-        Character character = new Character(200,200,5,5);
         Inventory bow = new Bow(15,15);
         character.addItem(bow);
         assertEquals(20,character.getArm(bow));
@@ -70,7 +72,6 @@ public class Character_will {
     }
     @Test
     public void get_Armor() {
-        Character character = new Character(200,200,5,5);
         Inventory armor = new Armor(15,15);
         character.addItem(armor);
         assertEquals(20,character.getArmor(armor));
@@ -78,7 +79,6 @@ public class Character_will {
     }
     @Test
     public void attack(){
-        Character character = new Character(200,200,5,5);
         Enemy dog = new Dog(200,5,0);
         assertEquals(195,character.attackEnemy(dog));
     }
--- test/game/Enemy_will.java
+++ test/game/Enemy_will.java
@@ -1,16 +1,24 @@
 package game;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class Enemy_will {
+
+    private Character character;
+
+    @Before
+    public void setUp() throws Exception {
+        character = new Character(200,200,5,5);
+    }
+
     @Test
     public void Attack_character(){
-        Enemy enemy = new Dog(200,5,5);
-        Character character = new Character(200,200,5,5);
-        assertEquals(200,enemy.attack(character));
+        Enemy dog = new Dog(200,5,5);
+        assertEquals(200,dog.attack(character));
 
     }
 }
